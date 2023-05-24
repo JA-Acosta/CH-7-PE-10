d9ac906f62e101ac97698dbd2a200f22a8fc05f3
JAAR <joe.aaron.acosta@outlook.com>
2023-05-24 16:10:14 -0400

    Version 1: Program Skeleton

---
+++ main.cpp
@@ -0,0 +1,37 @@
+// JAAR
+// 05/24/2023
+// C++ Primer Plus 6th Ed
+// CH 7 Program Ex 10
+// Version 1
+ 
+// Updates:
+
+// Future goals:
+
+// Potential Use cases/errors:
+
+// ------------------------------ Prompt---------------------------------------
+// design a function double calculate(double, double, ptr function) that will
+// return the double from the function it points to as it's return statement.
+// and will pass both of it's arguments to the function.
+// The function it points to will be double function(double, double)  
+//
+// ------------------------------ Prompt---------------------------------------
+
+// Headers:
+#include <iostream>
+#include <cctype>
+#include <string>
+
+// Global Constants and Structures:
+
+
+// Prototypes:
+double calculate(double x, double y, double* ptr_f(double x, double y));
+double add(double x, double y);
+
+int main()
+{
+
+
+}
