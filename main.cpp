22d740871e2578d6c5de704556df1168718971d2
JAAR <joe.aaron.acosta@outlook.com>
2023-05-25 13:18:52 -0400

    Version 3

--- main.cpp
+++ main.cpp
@@ -2,17 +2,13 @@
 // 05/25/2023
 // C++ Primer Plus 6th Ed
 // CH 7 Program Ex 10
-// Version 2
+// Version 3
  
 // Updates:
-// +calculate function
-// +add function
-// +divide function
-// +subtract function
-// +multiply function
+// +userInput function
 
 // Future goals:
-// print out the question and will then print out the solution.
+// print the questions and solutions to a txt file.
 
 // Potential Use cases/errors:
 
@@ -42,9 +38,23 @@ double add(double x, double y);
 double multiply(double x, double y);
 double subtract(double x, double y);
 double divide(double x, double y);
+bool userInput(double* x, double* y);
 
 int main()
 {
+	cout << "Enter consecutive pairs of numbers and I will calculate the solution"
+		" for their summation, multiplication, division, and subtraction.\n";
+	cout << "\tNote: For the subtraction and division function, the first value will be "
+		"acted on by the second value.\n" << endl;
+	cout << "To terminate the program input a non-numerical value for either"
+		" x or y.\n";
+	double x;
+	double y;
+
+	while (userInput(&x, &y))
+	{
+		cout << "Entered this loop\n";
+	}
 }
 
 // Calculates the answer
@@ -85,4 +95,20 @@ double subtract(double x, double y)
 double divide(double x, double y)
 {
 	return x / y;
+}
+
+// Asks the user for two values and if the user doesn't enter a valid number
+// will return false otherwise returns true.
+// !assume the user won't input a line
+// Param: double x, double y
+// Return bool
+bool userInput(double* x, double* y)
+{
+	cout << "Enter a value for x : ";
+	if (!(cin >> *x))
+		return false;
+	cout << "Enter a value for y : ";
+	if (!(cin >> *y))
+		return false;
+	return true;
 }
