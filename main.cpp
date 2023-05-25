66bdcbff97d262968aa628032a0ecadca85b723f
JAAR <joe.aaron.acosta@outlook.com>
2023-05-25 12:58:11 -0400

    Version 2: Include Functions

--- main.cpp
+++ main.cpp
@@ -1,27 +1,37 @@
 // JAAR
-// 05/24/2023
+// 05/25/2023
 // C++ Primer Plus 6th Ed
 // CH 7 Program Ex 10
-// Version 1
+// Version 2
  
 // Updates:
+// +calculate function
+// +add function
+// +divide function
+// +subtract function
+// +multiply function
 
 // Future goals:
+// print out the question and will then print out the solution.
 
 // Potential Use cases/errors:
 
 // ------------------------------ Prompt---------------------------------------
-// design a function double calculate(double, double, ptr function) that will
-// return the double from the function it points to as it's return statement.
-// and will pass both of it's arguments to the function.
-// The function it points to will be double function(double, double)  
-//
+// Design a function that calculates the solution for various arguments using
+// a function calculate that is a pointer to other functions.
+// 
+// !Optional
+// Create an array of pointers to add()-style function and use a loop to
+// successively apply calculate to a series of functions.
 // ------------------------------ Prompt---------------------------------------
+
 
 // Headers:
 #include <iostream>
 #include <cctype>
 #include <string>
+
+using namespace std;
 
 // Global Constants and Structures:
 
@@ -29,9 +39,50 @@
 // Prototypes:
 double calculate(double x, double y, double* ptr_f(double x, double y));
 double add(double x, double y);
+double multiply(double x, double y);
+double subtract(double x, double y);
+double divide(double x, double y);
 
 int main()
 {
+}
 
+// Calculates the answer
+// Param: double x, double y, double * ptr_f
+// Return: double
+double calculate(double x, double y, double* ptr_f(double x, double y))
+{
+	return *ptr_f(x, y);
+}
 
+// Adds two numbers and returns the solution.
+// Param: double x, double y
+// Return: double
+double add(double x, double y)
+{
+	return x + y;
+}
+
+// Multiplies two numbers and returns the solution.
+// Param: double x, double y
+// Return: double
+double multiply(double x, double y)
+{
+	return x * y;
+}
+
+// Subtracts the second number passed from the first and returns the solution.
+// Param: double x, double y
+// Return: double
+double subtract(double x, double y)
+{
+	return x - y;
+}
+
+// Divides the first number passed by the second number and returns the solution.
+// Param: double x, double y
+// Return: double
+double divide(double x, double y)
+{
+	return x / y;
 }
